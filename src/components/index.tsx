eeff4e6e65b196865584ce9e85eae1624970dc8a
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-12 01:44:52 +0500

    Components

---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import Burger from "./Burger/Burger";
+import VerticalSlider from "./VerticalSlider/VerticalSlider";
+import Form from "./Form/Form";
+import Grid from "./Grid/Grid";
+
+export {
+    Burger,
+    VerticalSlider,
+    Form,
+    Grid
+};
