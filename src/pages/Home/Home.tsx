2dc55a64b203de8a421f84bf20d87a1a1f8d1c02
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-11 23:45:38 +0500

    Home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import { VerticalSlider } from "../../components";
+
+const Home = () => {
+    return (
+        <section id="home">
+            <VerticalSlider/>
+            <div className="container">
+                <div className="content">
+                    <div className="heading">We create <span>home</span> more aesthetic</div>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default Home;
