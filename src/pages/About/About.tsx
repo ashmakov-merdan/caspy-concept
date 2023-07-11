0a743df945c1de820e8281533e38c94c3d3f3e12
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-12 00:41:39 +0500

    About page

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,19 @@
+import "./about.scss";
+
+const About = () => {
+    return (
+        <section id="about">
+            <div className="container">
+                <div className="content">
+                    <h1 className="heading">We create ingenious designs that are truly captivating</h1>
+                    <div className="description">
+                        <p>Our mission is to create environments that deliver on every level. Aesthetic, practical, commercial, experiential. We achieve this by applying intelligence to design.</p>
+                        <p>By developing a personal vision for every project, setting the most unpromising standarts and working side by side with our clients to bring that vision to life. It's an approach that has cemented our reputation as world-class designers of luxury hotels, resorts and private residences.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default About;
