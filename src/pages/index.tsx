2cd9dacf7e29a6c32061603136f1f5120b62f1f4
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-12 01:47:10 +0500

    Pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Home from "./Home/Home";
+import Portfolio from "./Portfolio/Portfolio";
+import Services from "./Services/Services";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+
+export {
+    Home,
+    Portfolio,
+    Services,
+    About,
+    Contact
+};
