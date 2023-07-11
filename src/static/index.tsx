f482d86e840576f4aec3f3901104fe8d25fc8fb9
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-12 01:11:28 +0500

    Static database

---
+++ src/static/index.tsx
@@ -0,0 +1,109 @@
+import carousel1 from "../assets/images/carousel-1.jpg";
+import carousel2 from "../assets/images/carousel-2.jpg";
+import project1 from "../assets/images/project-1.jpg";
+import project2 from "../assets/images/project-2.jpg";
+import project3 from "../assets/images/project-3.jpg";
+import project4 from "../assets/images/project-4.jpg";
+import project5 from "../assets/images/project-5.jpg";
+import project6 from "../assets/images/project-6.jpg";
+import project7 from "../assets/images/project-7.jpg";
+import project8 from "../assets/images/project-8.jpg";
+import project9 from "../assets/images/project-9.jpg";
+
+export const navigation = [
+    {
+        title: 'home',
+        path: '/'
+    },
+    {
+        title: 'portfolio',
+        path: '/portfolio'
+    },
+    {
+        title: 'about us',
+        path: '/about'
+    },
+    {
+        title: 'services',
+        path: '/services'
+    },
+    {
+        title: 'contact',
+        path: '/contact'
+    }
+];
+
+export const carousel = [
+    {
+        img: carousel1
+    },
+    {
+        img: carousel2
+    },
+    {
+        img: carousel1
+    },
+    {
+        img: carousel2
+    }
+];
+
+export const services = [
+    {
+        title: "Interior design"
+    },
+    {
+        title: "Exterior design"
+    },
+    {
+        title: "Landshaft design"
+    },
+    {
+        title: "Architecture"
+    },
+    {
+        title: "Schematic design"
+    },
+    {
+        title: "Turnkey repair"
+    },
+    {
+        title: "Author's supervision"
+    },
+    {
+        title: "Exhibition stands"
+    },
+    {
+        title: "Restaurants & Offices"
+    }
+];
+
+export const projects = [
+    {
+        img: project1
+    },
+    {
+        img: project2
+    },
+    {
+        img: project3
+    },
+    {
+        img: project4
+    },
+    {
+        img: project5
+    },
+    {
+        img: project6
+    },
+    {
+        img: project7
+    },
+    {
+        img: project8
+    },
+    {
+        img: project9
+    }
+];
