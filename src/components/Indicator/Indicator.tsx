01783059692186dae56ab73811058e163c545909
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-12 12:03:46 +0500

    Some changes on Indicator

--- src/components/Indicator/Indicator.tsx
+++ src/components/Indicator/Indicator.tsx
@@ -1,19 +1,27 @@
 import "./indicator.scss";
 
 type IndicatorProps = {
-    pages: number[],
-    active: number,
-    onClick: (page: string) => void
+    pages: {
+        img: string
+    }[],
+    index: number,
+    setIndex: any
 }
-const Indicator = ({pages, active, onClick}: IndicatorProps) => {
+
+const Indicator = ({pages, index, setIndex}: IndicatorProps) => {
+
+    const handleChange = (i: number, img: string) => {
+        setIndex(i);
+    }
     return (
         <div className="indicator">
-            { pages.map((page) => {
-                const isActivePage: boolean = active === page;
-                return (
-                    <button key={page} onClick={() => onClick(page.toString())} style={{opacity: isActivePage ? '100%' : '50%'}}>0{page + 1}</button>
-                )
-            })}
+            {
+                pages.map((page, i) => {
+                    const { img } = page;
+                    const isActivePage = i === index;
+                    return <button key={i} style={{opacity: isActivePage ? '100%' : '50%'}} onClick={() => handleChange(i, img)}>0{i + 1}</button>
+                })
+            }
         </div>
     )
 };
