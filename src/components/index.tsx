aa578019930b0d5b51b5d616f9aacab20c5154b3
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-12 11:02:33 +0500

    Added SliderItem

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,10 +2,12 @@ import Burger from "./Burger/Burger";
 import VerticalSlider from "./VerticalSlider/VerticalSlider";
 import Form from "./Form/Form";
 import Grid from "./Grid/Grid";
+import SliderItem from "./VerticalSlider/SliderItem";
 
 export {
     Burger,
     VerticalSlider,
     Form,
-    Grid
+    Grid,
+    SliderItem
 };
